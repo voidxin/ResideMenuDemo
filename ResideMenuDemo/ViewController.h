f1ae76c82fe6749c370791032b65e35e6a47198f
张新 <zhangxin@zhangxindemacbook-pro.local>
2016-05-14 01:06:36 +0800

    Initial Commit

---
+++ ResideMenuDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ResideMenuDemo
+//
+//  Created by 张新 on 16/5/14.
+//  Copyright © 2016年 zhangxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
